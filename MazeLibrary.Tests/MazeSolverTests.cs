4d19a9b799466624389e0b8452501f64388e0bb2
MaxArt1405 <kondratovich4444@gmail.com>
2018-10-19 21:26:13 +0300

    Finish this task

--- MazeLibrary.Tests/MazeSolverTests.cs
+++ MazeLibrary.Tests/MazeSolverTests.cs
@@ -1,11 +1,26 @@
-using System;
+﻿using System;
 using NUnit.Framework;
+using System.Collections.Generic;
+using MazeLibrary;
 
 namespace MazeLibrary.Tests
 {
     [TestFixture]
     public class MazeSolverTests
     {
+        private static readonly List<Point> List = new List<Point>
+        {
+            new Point(3, 5),
+            new Point(3, 4),
+            new Point(3, 3),
+            new Point(4, 3),
+            new Point(4, 2),
+            new Point(4, 1),
+            new Point(3, 1),
+            new Point(2, 1),
+            new Point(1, 1),
+            new Point(1, 0)};
+
         private readonly int[] startXs = { 3, 0, 1, 0 };
 
         private readonly int[] startYs = { 5, 4, 0, 1 };
@@ -20,7 +35,46 @@ namespace MazeLibrary.Tests
                 { -1,  0, -1,  0,  0,  0 },
                 { -1,  0,  0,  0, -1, -1 },
                 { -1, -1, -1, -1, -1, -1 }
-            }         
+            },
+            new int[,]
+            {
+                { -1, -1, -1, -1,  0, -1 },
+                {  0,  0,  0, -1,  0, -1 },
+                { -1,  0, -1, -1,  0, -1 },
+                { -1,  0, -1,  0,  0, -1 },
+                { -1,  0,  0,  0, -1, -1 },
+                { -1, -1, -1, -1, -1, -1 }
+            },
+            new int[,]
+            {
+                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+                {  0,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0, -1 },
+                { -1,  0, -1,  0, -1, -1,  0,  0, -1, -1,  0, -1 },
+                { -1,  0, -1,  0,  0, -1,  0,  0,  0,  0,  0, -1 },
+                { -1,  0, -1, -1,  0, -1, -1, -1, -1, -1, -1, -1 },
+                { -1,  0, -1,  0,  0, -1,  0, -1,  0,  0,  0, -1 },
+                { -1,  0, -1,  0, -1, -1,  0,  0,  0, -1,  0, -1 },
+                { -1,  0, -1,  0,  0,  0,  0, -1, -1, -1,  0, -1 },
+                { -1,  0, -1,  0, -1,  0,  0, -1,  0, -1,  0, -1 },
+                { -1,  0, -1, -1, -1, -1,  0, -1,  0, -1,  0, -1 },
+                { -1,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0 },
+                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
+            },
+            new int[,]
+            {
+                { -1,  0, -1, -1, -1, -1, -1, -1, -1,  0, -1, -1 },
+                { -1,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0, -1 },
+                { -1,  0, -1,  0, -1, -1,  0,  0, -1, -1,  0, -1 },
+                { -1,  0, -1,  0,  0, -1,  0,  0,  0,  0,  0, -1 },
+                { -1,  0, -1, -1,  0, -1, -1, -1, -1, -1,  0, -1 },
+                { -1,  0, -1,  0,  0, -1,  0, -1,  0,  0,  0, -1 },
+                { -1,  0, -1,  0, -1, -1,  0,  0,  0, -1,  0, -1 },
+                { -1,  0, -1,  0,  0,  0,  0, -1, -1, -1,  0, -1 },
+                { -1,  0, -1,  0, -1,  0,  0, -1,  0, -1,  0, -1 },
+                { -1,  0, -1, -1, -1, -1,  0, -1,  0, -1,  0, -1 },
+                { -1,  0,  0,  0,  0,  0,  0, -1,  0,  0,  0, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
+            }
         };
 
         private readonly int[][,] result = new int[][,]
@@ -33,6 +87,45 @@ namespace MazeLibrary.Tests
                 { -1,  7, -1,  3,  2,  1 },
                 { -1,  6,  5,  4, -1, -1 },
                 { -1, -1, -1, -1, -1, -1 }
+            },
+            new int[,]
+            {
+                { -1, -1, -1, -1,  1, -1 },
+                { 12, 11,  0, -1,  2, -1 },
+                { -1, 10, -1, -1,  3, -1 },
+                { -1,  9, -1,  5,  4, -1 },
+                { -1,  8,  7,  6, -1, -1 },
+                { -1, -1, -1, -1, -1, -1 }
+            },
+            new int[,]
+            {
+                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+                {  1,  2, -1,  0,  0,  0,  0,  0, -1,  0,  0, -1 },
+                { -1,  3, -1,  0, -1, -1,  0,  0, -1, -1,  0, -1 },
+                { -1,  4, -1,  0,  0, -1,  0,  0,  0,  0,  0, -1 },
+                { -1,  5, -1, -1,  0, -1, -1, -1, -1, -1, -1, -1 },
+                { -1,  6, -1,  0,  0, -1,  0, -1, 23, 24, 25, -1 },
+                { -1,  7, -1,  0, -1, -1, 20, 21, 22, -1, 26, -1 },
+                { -1,  8, -1,  0,  0,  0, 19, -1, -1, -1, 27, -1 },
+                { -1,  9, -1,  0, -1,  0, 18, -1,  0, -1, 28, -1 },
+                { -1, 10, -1, -1, -1, -1, 17, -1,  0, -1, 29, -1 },
+                { -1, 11, 12, 13, 14, 15, 16, -1,  0,  0, 30, 31 },
+                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
+            },
+            new int[,]
+            {
+                { -1,  1, -1, -1, -1, -1, -1, -1, -1, 31, -1, -1 },
+                { -1,  2, -1,  0,  0,  0,  0,  0, -1, 30, 29, -1 },
+                { -1,  3, -1,  0, -1, -1,  0,  0, -1, -1, 28, -1 },
+                { -1,  4, -1,  0,  0, -1,  0,  0,  0,  0, 27, -1 },
+                { -1,  5, -1, -1,  0, -1, -1, -1, -1, -1, 26, -1 },
+                { -1,  6, -1,  0,  0, -1,  0, -1, 23, 24, 25, -1 },
+                { -1,  7, -1,  0, -1, -1, 20, 21, 22, -1,  0, -1 },
+                { -1,  8, -1,  0,  0,  0, 19, -1, -1, -1,  0, -1 },
+                { -1,  9, -1,  0, -1,  0, 18, -1,  0, -1,  0, -1 },
+                { -1, 10, -1, -1, -1, -1, 17, -1,  0, -1,  0, -1 },
+                { -1, 11, 12, 13, 14, 15, 16, -1,  0,  0,  0, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
             }
         };
 
@@ -42,11 +135,11 @@ namespace MazeLibrary.Tests
 
         [Test]
         public void MazeSolverConstructor_WithInvalidStartIndexX_ThrowsArgumentException()
-            => Assert.Throws<ArgumentException>(() => new MazeSolver(sourceData[0], -12, 2));
+            => Assert.Throws<ArgumentException>(() => new MazeSolver(sourceData[1], -12, 2));
 
         [Test]
         public void MazeSolverConstructor_WithInvalidStartIndexY_ThrowsArgumentException()
-            => Assert.Throws<ArgumentException>(() => new MazeSolver(sourceData[0], 0, -2));
+            => Assert.Throws<ArgumentException>(() => new MazeSolver(sourceData[1], 0, -2));
 
         [Test]
         public void PassMaze_SuccessfulTests()
@@ -59,15 +152,52 @@ namespace MazeLibrary.Tests
 
                 if (!MatrixAreEquals(solver.MazeWithPass(), result[i]))
                 {
-                    //TODO
+                    Assert.Fail();
                 }
+
+                Assert.Pass();
             }
         }
-        private static bool MatrixAreEquals(int[,] lhs, int[,] rhs)
+
+        private static bool PointAreEquals(List<Point> lhs, List<Point> rhs)
         {
-            bool answer = lhs.Equals(rhs);
-            return answer;
+            if ((lhs.Count != rhs.Count))
+            {
+                return false;
+            }
+            for (int i = 0; i < lhs.Count; i++)
+            {
+                if (lhs[i].X != rhs[i].X || lhs[i].Y != rhs[i].Y)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
+        private static bool MatrixAreEquals(int[,] lhs, int[,] rhs)
+        {
+            int lhsLength = lhs.GetUpperBound(0) + 1;
+            int lhsWidth = lhs.Length / lhsLength;
+            int rhsLength = rhs.GetUpperBound(0) + 1;
+            int rhsWidth = rhs.Length / rhsLength;
+
+            if ((lhsLength != rhsLength) || (lhsWidth != rhsWidth))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < lhsLength; i++)
+            {
+                for (int j = 0; j < lhsWidth; j++)
+                {
+                    if (lhs[i, j] != rhs[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
--- MazeLibrary/MazeSolver.cs
+++ MazeLibrary/MazeSolver.cs
@@ -1,63 +1,109 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MazeLibrary
 {
     public class MazeSolver
     {
+        private readonly int startY;
+        private readonly int startX;
+        private readonly int _dimension;
+        private readonly int[,] mazeModel;
+
         public MazeSolver(int[,] mazeModel, int startX, int startY)
         {
+            this.startY = startY;
+            this.startX = startX;
             Valid(mazeModel, startX, startY);
-
-            int width = mazeModel.GetUpperBound(0);
-            int height = mazeModel.GetUpperBound(1);
-
-            for(int i = 0; i < width; i++)
-            {
-                for(int j = 0; j < height; j++)
-                {
-                    
-                }
-            }
-            recursiveSolve(mazeModel, startX, startY);
-        }
-        private int endX, endY;
-        private int[][] correct = new int[6][];
-        private bool recursiveSolve(int[,] maze, int x, int y)
-        {
-            bool solved = false;
-            if (x == endX && y == endY)
-            {
-                solved = true;
-            }
-            if (maze[x + 1,y] == -1)
-            {
-                recursiveSolve(maze,x + 1, y);
-            }
-            else if (maze[x,y + 1] == -1)
-            {
-                recursiveSolve(maze,x, y -1);
-            }
-            else if (maze[x - 1,y] == -1)
-            {
-                recursiveSolve(maze,x - 1, y);
-            }
-            else if (maze[x,y - 1] == -1)
-            {
-                recursiveSolve(maze,x,y - 1);
-            }
-            return solved;
+            _dimension = (int)Math.Sqrt(mazeModel.Length);
+            this.mazeModel = new int[_dimension, _dimension];
+            Array.Copy(mazeModel, this.mazeModel, mazeModel.Length);
         }
         public int[,] MazeWithPass()
         {
-            throw new NotImplementedException();
+            return (int[,])mazeModel.Clone();
         }
-
-        public void PassMaze()
+        public List<Point> PointsFromEntranceToExit()
         {
+            var pointList = new List<Point>();
+            int max = 0;
+            for (int i = 0; i < _dimension; i++)
+            {
+                for (int j = 0; j < _dimension; j++)
+                {
+                    if (mazeModel[i, j] > max)
+                    {
+                        max = mazeModel[i, j];
+                    }
+                }
+            }
+            for (int i = 1; i <= max; i++)
+            {
+                for (int j = 0; j < _dimension; j++)
+                {
+                    for (int k = 0; k < _dimension; k++)
+                    {
+                        if (i == mazeModel[j, k])
+                        {
+                            pointList.Add(new Point(j, k));
+
+                        }
+                    }
+                }
+            }
+            return pointList;
+        }
+        public void PassMaze() => FindAWay();
+        private void FindAWay()
+        {
+            bool[,] mapBools = new bool[_dimension, _dimension];
+            for (int i = 0; i < _dimension; i++)
+            {
+                for (int j = 0; j < _dimension; j++)
+                {
+                    mapBools[i, j] = (mazeModel[i, j] == 0);
+                }
+            }
+            int variable = 1;
+            NextStep(mapBools, startX, startY, variable);
+        }
+        private bool NextStep(bool[,] map, int x, int y, int variable)
+        {
+            if (!(x == startX && y == startY))
+            {
+                if (x < 0 || y < 0)
+                {
+                    return true;
+                }
+                if (x >= _dimension || y >= _dimension)
+                {
+                    return true;
+                }
+                if (!map[x, y])
+                {
+                    return false;
+                }
+            }
+            map[x, y] = false;
+
+            var up = NextStep(map, x + 1, y, variable + 1);
+            var down = NextStep(map, x - 1, y, variable + 1);
+            var right = NextStep(map, x, y + 1, variable + 1);
+            var left = NextStep(map, x, y - 1, variable + 1);
+
+            if (up || down || right || left)
+            {
+                mazeModel[x, y] = variable;
+                return true;
+            }
+            return false;
         }
         private void Valid(int[,] mazeModel, int startX, int startY)
         {
-            if(mazeModel == null)
+            if (mazeModel == null)
             {
                 throw new ArgumentNullException($"The {nameof(mazeModel)} can't be null!");
             }
@@ -70,5 +116,15 @@ namespace MazeLibrary
                 throw new ArgumentException($"The start point{nameof(startY)} is invalid!");
             }
         }
+    }
+    public struct Point
+    {
+        public Point(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
